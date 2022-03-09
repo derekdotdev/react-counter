8732e17fec02cf9b43dbeb5bdc1404b1fe7f338d
Derek DiLeo <derek@Dereks-Mac-mini.local>
2022-03-09 17:28:57 -0500

    Implement Logout to remove User Auth Token from Session Storage.

--- src/components/todo/AuthenticationService.js
+++ src/components/todo/AuthenticationService.js
@@ -4,6 +4,10 @@ class AuthenticationService {
         console.log('Registered successful login')
         sessionStorage.setItem('authenticatedUser', username);
     }
+
+    logout() {
+        sessionStorage.removeItem('authenticatedUser');
+    }
 }
 
 export default new AuthenticationService()
