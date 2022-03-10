42c2a18132edf8615fa2e2ffd35848d7e1df911b
Derek DiLeo <derek@Dereks-Mac-mini.local>
2022-03-10 13:00:32 -0500

    Create API to handle HTTP requests from front end to back end application. Create methods to handle response.data and error.data and display to user.

--- src/api/todo/HelloWorldService.js
+++ src/api/todo/HelloWorldService.js
@@ -5,6 +5,15 @@ class HelloWorldService {
         return axios.get('http://localhost:8080/hello-world')
     }
 
+    executeHelloWorldBeanService() {
+        return axios.get('http://localhost:8080/hello-world-bean')
+    }
+    
+    // Notice the use of 'tick' character instead of single or double quotes
+    executeHelloWorldPathVariableService(name) {
+        return axios.get(`http://localhost:8080/hello-world/path-variable/${name}`)
+    }
+
 }
 
 export default new HelloWorldService()
--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -8,17 +8,25 @@ class WelcomeComponent extends Component {
         super(props)
 
         this.retrieveWelcomeMessage = this.retrieveWelcomeMessage.bind(this)
+
         
         this.state = {
-            welcomeMessage : ''
+            welcomeMessage : '',
+            errorMessage : '',
+            hasHttpError: false
         }
 
         this.handleSuccessfulResponse = this.handleSuccessfulResponse.bind(this)
+        this.handleError = this.handleError.bind(this)
     }
     
     render() {
         return (
             <>
+                <div>
+                    {this.state.hasHttpError && <div className="alert alert-warning">{this.state.errorMessage}</div>} 
+                </div>
+                {<div className="errorMessage"></div>}
                 <h1>Welcome!</h1>
                 <div className="container">
                     Welcome {this.props.params.name}. 
@@ -36,14 +44,34 @@ class WelcomeComponent extends Component {
     }
 
     retrieveWelcomeMessage() {
-        HelloWorldService.executeHelloWorldService()
-        .then(response => this.handleSuccessfulResponse(response))
-        // .catch(console.log('ERROR!'));
+        // API call which Returns String
+        // HelloWorldService.executeHelloWorldService()
+        // .then(response => this.handleSuccessfulResponse(response)) 
+
+        // API call which Returns JSON Object
+        // HelloWorldService.executeHelloWorldBeanService()
+        // .then(response => this.handleSuccessfulResponse(response))
+        // .catch(console.log('ERROR!'));   
+
+        // API call with Path Variable
+        HelloWorldService.executeHelloWorldPathVariableService(this.props.params.name)
+        .then( response => this.handleSuccessfulResponse(response))
+        .catch( error => this.handleError(error))
 
     }
 
     handleSuccessfulResponse(response) {
-        this.setState({welcomeMessage: response.data})
+        // (String returned)
+        // this.setState({welcomeMessage: response.data})
+
+        //Bean (Need to extract String message)
+        this.setState({welcomeMessage: response.data.message})
+    }
+
+    handleError(error) {
+        console.log(error.response)
+        this.setState({hasHttpError: true})
+        this.setState({errorMessage: error.response.data.message})
     }
 
 }
